f96d39257b77cb232c6c0ad738eedc6dfa3e56e6
santsa <santsa@gmail.com>
2026-09-25 14:31:10 +0000

    add chat model

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mediaasistant
 Repo test AI with spring
+
+
+## Orders codespace
+Shift + Ctrl + P
+Codespaces: Stop Current Codespace
+
+gh codespace stop
+gh codespace stop -r santsa/mediaasistant
--- src/main/java/com/santsa/mediaassistant/TestLlmCall.java
+++ src/main/java/com/santsa/mediaassistant/TestLlmCall.java
@@ -1,6 +1,10 @@
 package com.santsa.mediaassistant;
 
+import java.util.Objects;
+
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +17,18 @@ public class TestLlmCall implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        String response = chatModel.call("¿Qué es la fiebre?");
-        System.out.println("Response from LLM: " + response);
+        ChatResponse chatResponse = chatModel.call(new Prompt("¿Qué es la fiebre?"));
+
+        String content = Objects.requireNonNull(chatResponse.getResult()).getOutput().getText();
+        System.out.println("=== Response ===");
+        System.out.println(content);
+
+        System.out.println("\n=== Metadata ===");
+        System.out.println("Model" + chatResponse.getMetadata().getModel());
+        System.out.println("Tokens de entrada: " + chatResponse.getMetadata().getUsage().getPromptTokens());
+        System.out.println("Tokens de salida: " + chatResponse.getMetadata().getUsage().getCompletionTokens());
+        System.out.println("Tokens totales: " + chatResponse.getMetadata().getUsage().getTotalTokens());
+        System.out.println("Finish reason: " + chatResponse.getResult().getMetadata().getFinishReason());        
     }
 
 }
